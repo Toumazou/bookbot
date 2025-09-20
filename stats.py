0c3fe89b335470d41a12c42e17e3fc99e7dde83c
Toumazou <yiannistoumazou@gmail.com>
2025-09-20 15:40:54 +0100

    updated CH2 code

--- stats.py
+++ stats.py
@@ -8,41 +8,45 @@ def get_book_charcount(filepath):
         file_contents = f.read()
     file_contents_lower = file_contents.lower()
     dictionary = {
-            "a":0,
-            "b":0,
-            "c":0,
-            "d":0,
-            "e":0,
-            "f":0,
-            "g":0,
-            "h":0,
-            "i":0,
-            "j":0,
-            "k":0,
-            "l":0,
-            "m":0,
-            "n":0,
-            "o":0,
-            "p":0,
-            "q":0,
-            "r":0,
-            "s":0,
-            "t":0,
-            "u":0,
-            "v":0,
-            "w":0,
-            "x":0,
-            "y":0,
-            "z":0
-   }
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0,
+            "h": 0,
+            "i": 0,
+            "j": 0,
+            "k": 0,
+            "l": 0,
+            "m": 0,
+            "n": 0,
+            "o": 0,
+            "p": 0,
+            "q": 0,
+            "r": 0,
+            "s": 0,
+            "t": 0,
+            "u": 0,
+            "v": 0,
+            "w": 0,
+            "x": 0,
+            "y": 0,
+            "z": 0,
+            "æ": 0,
+            "â": 0,
+            "ê": 0,
+            "ë": 0,
+            "ô": 0
+    }
     for char in file_contents_lower:
-        for key in dictionary:
-            if key == char:
-                dictionary[key] = dictionary[key] + 1
+        if char in dictionary:
+            dictionary[char] = dictionary[char] + 1
     return dictionary
 
-# dictionary = get_book_charcount("/home/toum/workspace/github.com/Toumazou/bookbot/books/frankenstein.txt")
+dictionary = get_book_charcount("/home/toum/workspace/github.com/Toumazou/bookbot/books/frankenstein.txt")
 
-# for key in dictionary:
-    # print(f"{key}: {dictionary[key]}")
+for key in dictionary:
+    print(f"{key}: {dictionary[key]}")
 
